b891f68e1329da33f1673d8db2ca4cb0153507fa
Noah <noah.oberman@gmail.com>
2017-02-05 12:56:28 -0500

    finished repeat, generateBoard and rowColToIndex all tested and passed

--- src/app.js
+++ src/app.js
@@ -1,1 +1,5 @@
 // app.js
+const rev = require("./reversi.js");
+
+let board = rev.generateBoard;
+console.log(board(3,3));
--- src/reversi.js
+++ src/reversi.js
@@ -1,1 +1,47 @@
 // reversi.js
+
+function repeat(value, n){
+  let arr = [value];
+  for(let i = 0; i<n-1; i++){
+    arr.push(value);
+  }
+  return arr;
+}
+
+function generateBoard(rows, columns, initialCellValue = "num"){
+  const num = rows * columns;
+  let arr = [initialCellValue];
+  for(let i = 0; i < num - 1; i++){
+    arr.push(initialCellValue);
+    //console.log(arr[i]);
+  }
+  return arr;
+}
+
+function rowColToIndex(board, rowNumber, columnNumber){
+  const boardLength = Math.sqrt(board.length);
+  let index = rowNumber*boardLength+columnNumber;
+  return index;
+}
+
+function indexToRowCol(board ,i){
+
+}
+
+function setBoardCell(board, letter, row, col){
+
+}
+
+function algebraicToRowCol(algebraiNotation){
+
+}
+
+module.exports = {
+  repeat: repeat,
+  generateBoard: generateBoard,
+  rowColToIndex: rowColToIndex,
+  indexToRowCol: indexToRowCol,
+  setBoardCell: setBoardCell,
+  algebraicToRowCol: algebraicToRowCol,
+  
+}
